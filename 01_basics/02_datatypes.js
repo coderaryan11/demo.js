29e69067c512c096396835dc2a999417000b87d0
coderaryan11 <120705086+coderaryan11@users.noreply.github.com>
2023-09-20 20:22:04 +0000

    Datatpes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(hello)   we are using node.js not browser
+
+//code readibility is high priority
+
+let name = 'RAHUL'
+let age = 34
+let isLoggedIn = true
+let State;
+
+// number => 2 to power 53
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof null);
