2bf0ffd3ad07b5df2d2da3bb983a6cbe60e585d7
coderaryan11 <120705086+coderaryan11@users.noreply.github.com>
2023-09-20 20:39:33 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = 'karan@gmail.com'
+var accountpassword = '12345'
+accountCity ='Jaipur' //u can reserve a varibale memory with using var but this is not prefer to use. but we can declare
+
+let accountState;
+
+
+accountEmail = 'mohan@google.com'
+accountPassword = '54321'
+accountCity ='Goa'
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
